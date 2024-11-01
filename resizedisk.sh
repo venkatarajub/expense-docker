60356cba479de370339a94acd4de7c5242c14b09
Venkata <venkataraju.bandaru8@gmail.com>
2024-11-01 15:54:45 +0530

    expense-docker

--- resizedisk.sh
+++ resizedisk.sh
@@ -1,7 +1,9 @@
 #!?bin/bash
+df -hT
 lsblk
 sudo growpart /dev/nvme0n1 4
 sudo lvextend -l +50%FREE /dev/RootVG/rootVol
 sudo lvextend -l +50%FREE /dev/RootVG/varVol
 sudo xfs_growfs /
 sudo xfs_growfs /var
+df -hT
