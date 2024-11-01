77d04aff9ec33c61ed20ee33c92f90a6f60bc079
Venkata <venkataraju.bandaru8@gmail.com>
2024-11-01 14:52:53 +0530

    expense-docker

---
+++ dockerinstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ec2-user
+echo "Logout and Login again"
---
+++ resizedisk.sh
@@ -0,0 +1,7 @@
+#!?bin/bash
+lsblk
+sudo growpart /dev/nvme0n1 4
+sudo lvextend -l +50%FREE /dev/RootVG/rootVol
+sudo lvextend -l +50%FREE /dev/RootVG/varVol
+sudo xfs_growfs /
+sudo xfs_growfs /var
